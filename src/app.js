e7c8cd864c4911799631f9cd270d210dde5e61d3
Emmanuel Luis <ejluis2001@gmail.com>
2025-05-20 11:53:33 -0500

    Added CRUD operations

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create your GET Request Route Below:
 
 app.get("/restaurants", async (request, resposne) => {
@@ -14,6 +17,47 @@ app.get("/restaurants/:id", async (req, res, next) => {
     try {
         const restaurant = await Restaurant.findByPk(req.params.id);
         res.status(200).json(restaurant);
+    } catch (err) {
+        next(err);
+    }
+})
+
+app.post("/restaurants", async (req, res, next) => {
+    try {
+        const newRestaurant = await Restaurant.create(req.body);
+        res.status(200).json(newRestaurant);
+    } catch (err) {
+        next(err);
+    }
+})
+
+app.put("/restaurants/:id", async (req, res, next) => {
+    try {
+        const foundRestaurant = await Restaurant.findByPk(req.params.id);
+
+        if (foundRestaurant) {
+            await foundRestaurant.update(req.body);
+            res.status(200).json(foundRestaurant);
+        } else {
+            res.status(400).send(`Restaurant with ${req.params.id} not found`);
+        }
+    } catch (err) {
+        next(err);
+    }
+})
+
+app.delete("/restaurants/:id", async (req, res, next) => {
+    try {
+        let foundRestaurant = await Restaurant.findByPk(req.params.id);
+
+        if (foundRestaurant) {
+            foundRestaurant = await foundRestaurant.destroy();
+            console.log(foundRestaurant);
+            res.status(200).json(foundRestaurant);
+        } else {
+            res.status(400).send(`Restaurant with ${req.params.id} not found`);
+        }
+
     } catch (err) {
         next(err);
     }
